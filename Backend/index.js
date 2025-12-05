23d57f3c736aed221e24f82b18543c33d3ba26a1
omar-khaled67 <ok672005@gmail.com>
2025-12-05 14:11:39 +0200

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 8000;
 
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${process.env.FRONTEND_HOST}:${PORT}`);
+  console.log(Server running on ${process.env.FRONTEND_HOST || 'http://localhost'}:${PORT});
   dbConnection(); 
 });
