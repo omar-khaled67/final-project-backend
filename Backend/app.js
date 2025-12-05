1caa2c0703c7f486b187ce7ff019d684de93a5c8
omar-khaled67 <ok672005@gmail.com>
2025-12-05 16:57:02 +0200

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,36 +1,28 @@
-
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
+// Routes
 const authRoute = require("./routes/auth");
+const prospectRoutes = require("./routes/prospect");
+const donorRoutes = require("./routes/donor");
 
-
+// Middlewares
 app.use(cors({
   origin: process.env.FRONTEND_HOST, 
   credentials: true
 }));
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoute);
+app.use("/api/prospect", prospectRoutes);
+app.use("/api/donor", donorRoutes);
 
-
+// Health check
 app.get("/", (req, res) => {
   res.send("Blood Bridge Backend is running perfectly!");
 });
 
-const prospectRoutes = require("./routes/prospect");
-
-app.use("/api/prospect", prospectRoutes);
-
-
-const donorRoutes = require("./routes/donor");
-app.use("/api/donor", donorRoutes);
-
-
-
-
-
-
-
+// Export app only
 module.exports = app;
