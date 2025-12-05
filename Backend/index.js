0b8e4f6e948b91f4c342fd56f43a5745b0372640
omar-khaled67 <ok672005@gmail.com>
2025-12-05 14:12:52 +0200

    Fix template literal syntax in console log

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 8000;
 
 
 app.listen(PORT, () => {
-  console.log(Server running on ${process.env.FRONTEND_HOST || 'http://localhost'}:${PORT});
+  console.log(`Server running on ${process.env.FRONTEND_HOST || 'http://localhost'}:${PORT}`);
   dbConnection(); 
 });
